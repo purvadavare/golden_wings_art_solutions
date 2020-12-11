619944f43ec680a5b142f82768309cf805953001
purvadavare <purvadavare@gmail.com>
2020-12-11 14:36:36 +0530

    commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -22,9 +22,7 @@ $( '.links a' ).on( 'click', function(e){
   // Run a scroll animation to the position of the element which has the same id like the href value.
   $( 'html, body' ).animate({
 		scrollTop: $( href ).offset().top
-  }, '300' );
-	
-  // Prevent the browser from showing the attribute name of the clicked link in the address bar
+  }, '800' );
   e.preventDefault();
 
 });
